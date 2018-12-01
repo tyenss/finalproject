95825982ce19b847b2b04c12cc1510f03fbd5ddd
tyenss <yuuki.butler@gmail.com>
2018-12-01 10:26:19 -0500

    Started Project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import Welcome from './App';
 import * as serviceWorker from './serviceWorker';
+import Welcome from "./login";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+document.addEventListener("DOMContentLoaded", function(event) {
+    ReactDOM.render(<Welcome/>,document.getElementById("root"));
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
