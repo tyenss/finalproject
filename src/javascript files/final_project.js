86f3ae89bd9c2d10ee31f5d1149b02c7559b5a1a
rachelestherg <rachelgloer@yahoo.com>
2018-12-10 15:29:49 -0500

    final proj file changes

--- src/javascript files/final_project.js
+++ src/javascript files/final_project.js
@@ -10,12 +10,6 @@ function login(user,pass){
 
 	//console.log(user);
 	//console.log(pass);
-	var airline_id="";
-	var airline_1 = "";
-	var logo="";
-	var plane="";
-	var dest="";
-	var cost="";
 
 	$.ajax(root_url + "sessions",
 	       {
@@ -29,14 +23,7 @@ function login(user,pass){
 		   },
 		   success: () =>
 		   {
-			   build_airlines_interface();
-			   get_random_airline();
-			   get_airplane();
-			   get_flight();
-			   get_cost();
-			   //take in airline, airplane, destination, cost, logo
-			   ReactDOM.render(<MainPage airplane= {plane} airline={airline_1} destination={dest} logo={logo} cost={cost}/>,document.getElementById("root"));
-			   //get_airport(Lihue Airport);
+			   create_new_flight_info();
 		   },
 		   error: (jqxhr, status, error) => {
 		       alert(error);
@@ -44,6 +31,34 @@ function login(user,pass){
 	       
     });
 }
+
+function create_new_flight_info(){
+	var airline_id={id:""};
+	var airline_1 = {al:""};
+	var logo={lg:""};
+	var plane={pv:""};
+	var dest={dv:""};
+	var cost={cv:""};
+	var has_cost = {hc:false};
+	build_airlines_interface();
+	console.log("beFORE");
+	get_random_airline(airline_1,airline_id,logo,plane, dest,cost,has_cost,get_airplane);
+	
+	//console.log(dest);
+	return [airline_1,plane,cost,dest,logo];
+	//console.log("AFTERRRRRRR");
+	/*while(!has_cost.hc){
+		continue;
+	}*/
+	/*get_airplane(airline_id, plane);
+	get_flight(airline_id, dest);
+	get_cost(cost, has_cost);*/
+	
+	//take in airline, airplane, destination, cost, logo
+	//ReactDOM.render(<MainPage airplane= {plane.pv} airline={airline_1.al} destination={dest.dv} logo={logo.lg} cost={cost.cv}/>,document.getElementById("root"));
+	
+}
+
 
 var build_airlines_interface = function() {
     let body = $('body');
@@ -92,53 +107,66 @@ var build_airlines_interface = function() {
 
 }
 
-var get_random_airline = function(){
+var get_random_airline = function(airline_1,airline_id,logo,plane, dest,cost,has_cost,callback){
+	
 	$.ajax(root_url + "airlines",
 	   {
 	       type: 'GET',
 	       xhrFields: {withCredentials: true},
 	       success: (airlines) => {
-				var rand = Math.random() * (airlines.length -1);
-				window.airline_1 = airlines[rand].name;
-				window.airline_id = airlines[rand].id;
-				window.logo = airlines[rand].logo_url;
+				var rand = Math.floor(Math.random() * (airlines.length -1));
+				airline_1.al = airlines[rand].name;
+				airline_id.id = airlines[rand].id;
+				logo.lg = airlines[rand].logo_url;
+				callback(airline_id, airline_1, logo,plane,dest,cost,has_cost,get_flight);
+				
+				
 	       }
 	   });
 }
 
-var get_airplane = function(){
+var get_airplane = function(airline_id,airline_1, logo,plane, dest,cost,has_cost,callback){
 	$.ajax(root_url + "planes",
 	   {
 	       type: 'GET',
 	       xhrFields: {withCredentials: true},
 	       success: (planes) => {
 				for (let i=0; i<planes.length; i++) {
-					if(planes[i].airline_id == window.airline_id){
-						window.plane = planes[i].name; //last plane with matching airline is the one we will use
+					if(planes[i].airline_id === airline_id.id){
+						plane.pv = planes[i].name; //last plane with matching airline is the one we will use
 					}
 				}
-	       }
+				//console.log(plane);
+				callback(airline_id, plane, airline_1, logo, dest, cost, has_cost,get_cost);
+		   }
 	   });
 }
 
-var get_flight = function(){
+var get_flight = function(airline_id, plane, airline_1, logo, dest, cost,has_cost,callback){
 	$.ajax(root_url + "flights",
 	   {
 	       type: 'GET',
 	       xhrFields: {withCredentials: true},
 	       success: (flights) => {
 				for (let i=0; i<flights.length; i++) {
-					if(flights[i].airline_id == window.airline_id){
-						window.dest = flights[i].departure_id;
+					if(flights[i].airline_id === airline_id.id){
+						dest.dv = flights[i].departure_id;
 					}
 				}
+				callback(cost, plane,airline_1,dest,logo,has_cost);
 	       }
 	   });
-}
+};
 
 //randomly generate a cost for a flight
-var get_cost = function(){
-	window.cost = Math.random() * (700 - 200) + 200;
+var get_cost = function(cost, plane,airline_1,dest,logo,has_cost){
+	cost.cv = Math.random() * (700 - 200) + 200;
+	//callback(plane,airline_1,dest,logo,cost,has_cost);
 }
 
-export {login};
+/*var create_mainpage = function(plane,airline_1,dest,logo,cost,has_cost){
+	has_cost.hc=true;
+	return [airline_1,plane,cost,dest,logo];
+}*/
+
+export {login,create_new_flight_info};
